b8059e3ccbadb0f70df4d30cfdc0f564b3749e22
thisistheway369 <thisistheway369@icloud.com>
2020-02-03 14:34:21 -0800

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,31 +1,22 @@
-# passengers = {
-# suite_a: "Amanda Presley",
-# suite_b: "Seymour Hoffman",
-# suite_c: "Alfred Tennyson",
-# suite_d: "Charlie Chaplin",
-# suite_e: "Crumpet the Elf"
-# }
 
-def select_winner(passengers)
-  # add the code snippet here!
-  winner = ""
-  passengers.each do |suite,name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
-  end
-  winner
-end
-
-# birthday_kids = {
-#   "Timmy" => 9,
-#   "Sarah" => 6,
-#   "Amanda" => 27
-# }
+ birthday_kids = {
+   "Timmy" => 9,
+   "Sarah" => 6,
+   "Amanda" => 27
+ }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
   birthday_kids.each do |kids_name,age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+birthday_kids.each do |kids_name,age|
+  if age <= 12
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else
+    puts "You are too old for this."
+  end
+end
+end 
